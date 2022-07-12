d041d24bb141115be189a5566fa23df3c775adfc
Jorge <jpgiance@gmail.com>
2022-07-12 09:00:24 -0500

    power ball lotto simulation

---
+++ MedianOfTwoSortedArrays.java
@@ -0,0 +1,14 @@
+public class MedianOfTwoSortedArrays {
+        public static void main(String[] args) {
+        int[] arr1 = { 1, 2 };
+        int[] arr2 = { 3, 4 };
+
+        System.out.print(answer(arr1, arr2));
+    }
+
+        private static double answer(int[] arr1, int[] arr2) {
+            double median = 0;
+
+            return median;
+        }
+}
