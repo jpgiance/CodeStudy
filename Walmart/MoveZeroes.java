ac686458d7e2aa8138d58c835eafe1bd76c22615
Jorge <jpgiance@gmail.com>
2022-08-12 09:52:58 -0500

    move zeroes

--- Walmart/MoveZeroes.java
+++ Walmart/MoveZeroes.java
@@ -7,5 +7,36 @@ package Walmart;
  */
 
 public class MoveZeroes {
+
+    public static void main(String[] args) {
+        
+    
+        int[] nums = {0, 1, 0, 3, 12};
+
+        answer(nums);
+
+        for ( int i : nums) {
+            System.out.print(i);
+
+        }
+        System.out.println();
+     
+    }
+
+    private static void answer(int[] nums) {
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                count++;
+            } else {
+                nums[i-count] = nums[i];
+            }
+        }
+
+        for (int i = 0; i < count; i++) {
+            nums[nums.length - 1 - i] = 0;
+        }
+    }
     
 }
